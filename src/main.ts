e9bb1c2e7c5e03cc21b026d60f6bf779ccee51d1
BenJilks <benjyjilks@gmail.com>
2023-12-04 16:05:37 +0000

    Add reset button.

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,8 @@ const smooth_life = new SmoothLife(canvas)
 smooth_life.set_initial_state()
 setInterval(() => smooth_life.draw_frame(), 1000 / 30)
 
+const reset_button = document.getElementById('reset-button')
+reset_button.addEventListener('click', () => {
+    smooth_life.set_initial_state()
+})
+
